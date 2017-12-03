54ef550ac2185b367cd3b9140a689d7542d60da9
dreakzss <wanghr166@gmail.com>
2017-12-03 02:24:19 -0500

    [1.0] only grab comment

--- forum_downloader/spiders/backup1.py
+++ forum_downloader/spiders/backup1.py
@@ -1,83 +1,134 @@
-import re
-import xlwt
-from timeit import timeit
-import scrapy
 import urllib.request
 from bs4 import BeautifulSoup
-from scrapy.http import Request
-from forum_downloader.items import ForumDownloaderItem
+import time
 
-#
+first_url = "http://www.miui.com/thread-11018798-5-1.html"
+#start_time = time
 
-class MySpider(scrapy.Spider):
-    name = 'forum_spider'
+def get(first_url):
 
-    wb = xlwt.Workbook()
-    sh = wb.add_sheet('A Test Sheet')
+    start_time = time.time()
 
-    comments_list = []
-    max_num =''
-    grab_pages = 0
+    page = urllib.request.urlopen(first_url)
+    html = page.read().decode('utf-8')
 
-    def get_first_url(self):
-        first_url = "http://www.miui.com/thread-11018798-1-1.html"
-        return first_url
+    soup = BeautifulSoup(html, "html.parser")
+    table_list = soup.select('.plhin')
 
-    def get_saved(self):
+    item_list = []
 
-        file = 'fd1.xls'
+    for table in table_list:
+        item = []
+        floor = get_floor(table)
+        author = get_author(table)
+        phone_type = get_phone_type(table)
+        miui_type = get_miui_type(table)
+        title = get_title(table)
 
-        return file
+        comment_list = get_comment(table)  # get_comment返回一个数组，如果有quote长度为2
+        quote = ''
+        if len(comment_list) > 1:
+            comment = comment_list[0]
+            quote = comment_list[1]
+        else:
+            comment = comment_list[0]
 
-    def start_requests(self):
+        item.append(floor)
+        item.append(author)
+        item.append(phone_type)
+        item.append(miui_type)
+        item.append(title)
+        item.append(comment)
+        item.append(quote)
 
-        first_url = self.get_first_url()
+        # print(str(len(item)))
 
-        page = urllib.request.urlopen(first_url)
-        html = page.read().decode('utf-8')
-        soup = BeautifulSoup(html, "html.parser")
-        last_page = soup.select('.last')
-        self.max_num = str(last_page[0].getText())[4:]
+        for j in item:  # 遍历item中的各元素
+            print('item: ' + str(j))
 
-        print('max_num: ' + self.max_num)
-
-        base_url = str(first_url)[:-8]
-        bash_url = str(first_url)[-7:]
-
-        #print('base_Url: ' + base_url) #First_Url: http://www.miui.com/thread-11397653-
-        #print('bash_Url: ' + bash_url)
-
-        #for num in range(1, int(max_num) + 1):
-        for num in range(1, 7):
-
-            url = base_url + str(num) + bash_url
-            yield scrapy.Request(url=url, callback=self.parse)
-            # print('url: ' + url)
-
-
-    def parse(self, response):
-        #print(response.text)
-        comments = BeautifulSoup(response.text, 'lxml').select('.t_f')
-        num = len(comments)
-        i = 0
-        while (i < num):
-            a = comments[i].getText()
-            print(a)
-            self.comments_list.append(a)
-            i = i + 1
-        self.grab_pages = self.grab_pages + 1
-        self.write()
+        item_list.append(item)
+        print("--- seconds ---" % (time.time() - start_time))
 
 
-    def write(self):
-        print('write2')
-        #while (self.grab_pages == int(self.max_num)):
-        if self.grab_pages == 6:
-
-            for k in range(len(self.comments_list)):
-                self.sh.write(k, 0, self.comments_list[k])
-            self.wb.save(self.get_saved())
+        # print('item: ' + str(j) + ' len: ' + str(len(j)))
 
 
-    #comments:一页的comments数
-    #comments_list:数据存放处
+# 抓取楼层
+def get_floor(table):
+    floor_1 = table.find('a', {'class': 'floors_1'})
+    floor_2 = table.find('a', {'class': 'floors_2'})
+    floor_3 = table.find('a', {'class': 'floors_3'})
+    floor_4 = table.find('a', {'class': 'floors_4'})
+    floor_normal = table.find('a', {'class': 'floors_normal'})
+    if floor_1:
+        return 1
+    elif floor_2:
+        return 2
+    elif floor_3:
+        return 3
+    elif floor_4:
+        return 4
+    else:
+        # print('floor: ' + floor_normal.contents[1].getText())
+        floor = int(floor_normal.contents[1].getText())
+    return floor
+
+
+def get_author(table):
+    author = table.find('a', {'class': 'xw1'}).getText()
+
+    return author
+
+
+def get_phone_type(table):
+    # contents[1],[3],[5],[7],[9]为空
+    dl_list = table.find('dl', {'class': 'pil cl'})
+    phone_type = dl_list.contents[5].getText()
+
+    return phone_type
+
+
+def get_miui_type(table):
+    # contents[1],[3],[5],[7],[9]为空
+    dl_list = table.find('dl', {'class': 'pil cl'})
+    miui_type = dl_list.contents[11].getText()
+
+    return miui_type
+
+
+def get_title(table):
+    div_list = table.find('div', {'class': 'pi'})
+    title = div_list.find_next_sibling().find_next_sibling().getText()
+
+    return title
+
+
+# get_comment返回一个数组，如果有quote长度为2
+def get_comment(table):
+    td = table.find('td', {'class': 't_f'})
+
+    # 清除掉<br/>
+    for e1 in td.findAll('br'):
+        e1.extract()
+
+    # 判断有无quote，有则拼接好添加至item最后，并从comment中删除
+    quote = ''
+    for quote_div in td.findAll('div', {'class': 'quote'}):
+
+        if quote_div:
+            quote = '( ' + quote_div.blockquote.contents[0].getText() + ' ) ' + quote_div.blockquote.contents[1]
+            quote_div.extract()
+            # print(quote)
+
+    comment = td.getText()
+    # print(comment)
+
+    comment_list = []
+    comment_list.append(comment)
+    if quote:
+        comment_list.append(quote)
+
+    return comment_list
+
+
+get(first_url)
